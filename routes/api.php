d93844645ea359dd024f5af934d0b3d1a670c33e
XeniaTkacheva <xenia1898@gmail.com>
2023-08-13 15:44:19 +0300

    remove trash

--- routes/api.php
+++ routes/api.php
@@ -22,12 +22,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::group([
-    'prefix' => 'auth',
-], function () {
-    Route::post('register', RegisterController::class)->withoutMiddleware(['auth:sanctum', 'verified']);
-});
-
-Route::group([
     'prefix' => 'v1',
 ], function () {
     Route::get('posts', PostController::class);
